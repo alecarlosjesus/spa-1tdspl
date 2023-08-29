8fce875b3d83a86c3bb09577c736c3f8cf9d54ca
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-29 08:35:13 -0300

    Leo Yuuki me salvou - add the components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Cabecalho from './components/Cabecalho'
+import Secao from './components/Secao'
+import Rodape from './components/Rodape'
+import viteLogo from './assets/vite.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+export default function App(){
+
+  let viteAtl = 'Vite Logo'
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Cabecalho />
+      <Secao  viteLogoProps={viteLogo} viteAtlProps={viteAtl}/>
+      <Rodape />
+      
 
-export default App
+    </>
+  );
+
+}
