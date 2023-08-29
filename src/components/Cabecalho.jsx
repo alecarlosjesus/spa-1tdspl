b4aa65c68437e5363b5cd844bd5573bae0bc4a09
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-29 10:06:33 -0300

    Solução props.children

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,23 +1,22 @@
-export default function Cabecalho() {
+import { createElement } from "react"
+
+export default function Cabecalho(props) {
     {
       /* Vamos criar um cabecalho com um header, um titulo h1 e 
         uma lista ul com itens com links*/
     }
+    let newItem;
   
     return(
       <>
           <header>
       <h1>Vite + React / coded by LeoYuuki</h1>
       <ul>
-        <li>
-          <a href="#">Item-1</a>
-        </li>
-        <li>
-          <a href="#">Item-2</a>
-        </li>
-        <li>
-          <a href="#">Item-3</a>
-        </li>
+            {props.children.map((item,indice) => {
+                createElement('li', {key: indice}, item);
+                return  item;
+            })
+        }
       </ul>
     </header>
       </>
