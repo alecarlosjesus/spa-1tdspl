666c1fa85a6f69e675a48887e382c240b0fdba23
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-12 08:40:45 -0300

    Retirando estilo inline.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,49 +2,43 @@ import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function Produtos() {
-
   return (
     <div>
-        <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
-        <p>Os Melhores Produtos do Mercado</p>
-        
-        <table style={{borderCollapse:"collapse" , border:"2px solid #ffffff"}}>
-            <thead>
-                <tr>
-                    <th style={{border:"2px solid #ffffff"}}>ID</th>
-                    <th style={{border:"2px solid #ffffff"}}>NOME</th>
-                    <th style={{border:"2px solid #ffffff"}}>DESCRIÇÃO</th>
-                    <th style={{border:"2px solid #ffffff"}}>PREÇO</th>
-                    <th style={{border:"2px solid #ffffff"}}>EDITAR</th>
+      <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
+      <p>Os Melhores Produtos do Mercado</p>
 
-                </tr>
-            </thead>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+            <th>EDITAR</th>
+          </tr>
+        </thead>
 
-            <tbody>
-                {ListaProdutos.map((produto,indice)=>(
+        <tbody>
+          {ListaProdutos.map((produto, indice) => (
+            <tr key={indice}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.desc}</td>
+              <td>{produto.preco}</td>
+              <td>
+                {" "}
+                <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-                      <tr key={indice}>
-                        <td style={{border:"2px solid #ffffff"}}>{produto.id}</td>
-                        <td style={{border:"2px solid #ffffff"}}>{produto.nome}</td>
-                        <td style={{border:"2px solid #ffffff"}}>{produto.desc}</td>
-                        <td style={{border:"2px solid #ffffff"}}>{produto.preco}</td>
-                        <td style={{border:"2px solid #ffffff"}}>
-                            <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
-                        </td>
-                      </tr>
-
-                ))}
-            </tbody>
-
-            <tfoot>
-                <tr>
-                    <td colSpan={3}>PRODUTOS</td>
-                </tr>
-            </tfoot>
-        </table>
-        
-
-
+        <tfoot>
+          <tr>
+            <td colSpan={3}>PRODUTOS</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-  )
+  );
 }
