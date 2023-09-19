40af39f92d5106a83bf934b88e55d9efb8a09fad
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-19 09:51:44 -0300

    Exemplo de useEffect 3x

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,12 +3,40 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import classes from "./Produtos.module.css";
 import {AiFillEdit as Editar} from "react-icons/ai";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
+
+  const [counter, setCounter] = useState(0);
+  document.title = "Lista de Produtos: " + counter;
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza sempre que ocorre alguma atualização no componente!");
+  });
+
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza apenas uma vez no carregamento do componente!");
+  },[]);
+
+  const [counter2, setCounter2] = useState(0);
+
+  useEffect(()=>{
+    console.log("Este useEffect renderiza apenas se o objeto/elemento/componente e ou variável sofrer uma atualização. Devemos colocar o item a ser monitorado no array de dependências: [ ]");
+  },[counter2]);
+
   return (
     <div>
       <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
       <p>Os Melhores Produtos do Mercado</p>
+
+      <div>
+        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
+      <div>
+        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+      </div>
 
       <table className={classes.estilo}>
         <thead className={classes.tableHeaders}>
