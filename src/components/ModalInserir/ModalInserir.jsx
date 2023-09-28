d7fcdb9441ee2e0a7c285c3b1d1c426718de3c40
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-28 08:57:54 -0300

    Form no modal inserir criado.

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -4,8 +4,36 @@ export default function ModalInserir(props) {
   if (props.open) {
     return (
       <div className={styles.container}>
-        <h1>Modal Inserir</h1>
+        <h1>CADASTRO DE PRODUTOS</h1>
         <button onClick={()=> props.setOpen(false)}>CLOSE-MODAL</button>
+
+        <div>
+          <form>
+            <fieldset>
+              <legend>Novo Produto</legend>
+              <div className="formInput">
+                <label htmlFor="idNome">Nome</label>
+                <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto."/>
+              </div>
+              <div className="formInput">
+                <label htmlFor="idDesc">Descrição</label>
+                <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto."/>
+              </div>
+              <div className="formInput">
+                <label htmlFor="idPreco">Preço</label>
+                <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto."/>
+              </div>
+              <div className="formInput">
+                <label htmlFor="idImg">Imagem</label>
+                <input type="url" name="img" id="idImg" placeholder="Digite a url da imagem."/>
+              </div>
+              <div className="formInput">
+                <button>CADASTRAR</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+
       </div>
     );
   }
